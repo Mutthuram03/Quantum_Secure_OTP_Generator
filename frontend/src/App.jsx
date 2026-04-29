5e22cbadd1651a46012ba74844c2db2c6f7ad4e9
Mutthuram03 <srmutthuram2007@gmail.com>
2026-04-29 14:25:41 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { motion, AnimatePresence } from 'framer-motion';
 
-const API_BASE = `http://${window.location.hostname}:5000`;
+const API_BASE = 'https://quantum-secure-otp-generator.onrender.com';
 
 const App = () => {
   const [showHome, setShowHome] = useState(true);
